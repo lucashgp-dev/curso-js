cad86a55068ba4fdc527e9b1f7b0bd0ef0371ef3
lucashgp-dev <lucashgp.l@hotmail.com>
2021-04-28 17:03:42 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 65
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if (vel>60){
+    console.log('Velocidade acima da permitida. Multado!')
+
+}
+console.log('Dirija sempre usando o cinto de segurança')
