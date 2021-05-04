a72bcc0f1bc05a39d2b63da3bd2a78809c755463
lucashgp-dev <lucashgp.l@hotmail.com>
2021-05-04 08:40:01 -0300

    ex016

---
+++ aula12ex/ex016/assets/script/script.js
@@ -0,0 +1,45 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados novamente!')
+    }
+    else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'homen'
+
+            if (idade >= 0 && idade <= 10) { img.setAttribute('src', './assets/img/bebe-m.png') }
+
+            else if (idade < 21) { img.setAttribute('src', './assets/img/jovem-m.png') }
+      
+            else if (idade < 50) { img.setAttribute('src', './assets/img/adulto-m.png') }
+
+            else { img.setAttribute('src',('src', './assets/img/idoso-m.png')) }
+        }
+
+
+        else if (fsex[1].checked) {
+            gênero = 'mulher'
+
+            if (idade >= 0 && idade <= 10) { img.setAttribute('src', './assets/img/bebe-f.png') }
+
+            else if (idade < 21) { img.setAttribute('src', './assets/img/jovem-f.png') }
+
+            else if (idade < 50) { img.setAttribute('src', './assets/img/adulto-f.png') }
+
+            else { img.setAttribute('src', './assets/img/idoso-f.png') }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
