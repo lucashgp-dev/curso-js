b7616ca3b89d096da99cc76266b90135634f6e66
lucashgp-dev <lucashgp.l@hotmail.com>
2021-05-05 12:21:08 -0300

    ex017

---
+++ aula14ex/ex017/assets/script/script.js
@@ -0,0 +1,40 @@
+function contador() {
+    let ini = document.getElementById('txtini')
+    let fim = document.getElementById('txtfim')
+    let pas = document.getElementById('txtpas')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        //window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = `Impossivel Contar!`
+
+    } else {
+
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}	`
+            }
+
+        }
+        else {
+            //Contagem decrescente  
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}	`
+            }
+
+        }
+
+        res.innerHTML += `\u{1F3C1}`
+    }
+
+}
